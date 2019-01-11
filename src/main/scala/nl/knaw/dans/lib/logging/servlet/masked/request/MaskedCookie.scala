d60d6faef4ce99dbb15e4ed54f662640bb50887d
Richard van Heest <richard.van.heest@dans.knaw.nl>
2019-01-11 10:46:46 +0100

    rename cookie formatter function name

--- src/main/scala/nl/knaw/dans/lib/logging/servlet/masked/request/MaskedCookie.scala
+++ src/main/scala/nl/knaw/dans/lib/logging/servlet/masked/request/MaskedCookie.scala
@@ -24,12 +24,12 @@ private[masked] trait MaskedCookie extends RequestLogExtensionBase {
   abstract override protected def formatHeader(header: HeaderMapEntry): HeaderMapEntry = {
     super.formatHeader(header) match {
       case (name, values) if name.toLowerCase == "cookie" =>
-        name -> values.map(formatCookieValue)
+        name -> values.map(formatCookieHeader)
       case otherwise => otherwise
     }
   }
 
-  private def formatCookieValue(value: String): String = {
+  private def formatCookieHeader(value: String): String = {
     val cookieName = value.replaceAll("=.*", "")
     val cookieValue = value.replaceAll(".*=", "")
     val maskedCookieValue = cookieValue .replaceAll("[^.]+", "****")// replace sequences of chars without dots
